cc481ee481f04374a45a7eef7e659d60cfe9cbf1
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2024-01-06 19:37:02 +0530

    Create 6)call_detail_schema.sql

---
+++ 6)call_detail_schema.sql
@@ -0,0 +1,46 @@
+# Consider the schema of the call detail table to partitioned primary index:
+#  CREATE TABLE calldetail (
+#  phone_number DECIMAL(10) NOT NULL,
+#  call_start TIMESTAMP,
+#  call_duration INTEGER,
+#  call_description VARCHAR(30))
+#  PRIMARY INDEX (phone_number, call_start);
+# Demonstrate the query against this table be optimized by partitioning its primary index 
+# using partitioning techniques.
+
+create database if not exists prog6;
+use prog6;
+
+create table ppi_call_detail (
+    phone_number decimal(10) not null,
+    call_start timestamp,
+    call_duration integer,
+    call_description varchar(30),
+    primary key (phone_number, call_start)
+)
+partition by range(unix_timestamp(call_start)) (
+    partition p0 values less than (unix_timestamp('2023-01-22 00:00:00')),
+    partition p1 values less than (unix_timestamp('2023-01-23 00:00:00'))
+);
+
+desc ppi_call_detail;
+
+create index idx on ppi_call_detail(phone_number, call_start);
+
+show index from ppi_call_detail;
+
+-- Insert some sample data
+insert into ppi_call_detail values
+(122434231, '2023-01-20 00:00:00', 50, 'FDF'),
+(122343243, '2023-01-22 10:00:00', 40, 'FFR'),
+(122434230, '2023-01-11 00:00:00', 50, 'FDF'),
+(122343242, '2023-01-10 10:00:00', 40, 'FFR'),
+(123232323, '2022-01-11 10:00:00', 30, 'eed');
+
+
+select * from ppi_call_detail;
+
+select * from ppi_call_detail partition (p0);
+
+select * from ppi_call_detail partition (p1);
+
